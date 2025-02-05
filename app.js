8bda6156c7b2f31203b526de2e393951bbf479c2
DanOrenday <hibiki_1718@msn.com>
2025-02-04 19:18:30 -0600

    Actualiza amigo secreto

--- app.js
+++ app.js
@@ -1,24 +1,67 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let texAreaAmigo = document.getElementById('amigo')
-let amigos = [];
-let amigoSecreto = generarAmigoSecreto;
 
-validarNombre()
+//Variables
 
-function validarNombre() {
-    amigos = texAreaAmigo.value;
-if (texAreaAmigo = ' ') {
-    alert('Por favor, inserte un nombre.')
-} else {
-    
-}
+let amigos = []; //Array de amigos
+
+
+
+//funciones
+
+//funcion para agregar amigos
+function agregarAmigo(){  //Utilizamos el boton agregarAmigo del html
+    let nombreAmigo //Nombre de amigo
+    nombreAmigo = document.getElementById("amigo").value;
+    amigos.push(nombreAmigo);
+    if(nombreAmigo === ""){ //Condicion si el nombre esta vacio
+        alert("Ingresa el nombre de un amigo");
+    }
+    //document.getElementById("listaAmigos").innerHTML = `<li>${amigos}</li>`;
+    mostrarLista();
+    limpiarCaja();
+    return;
 }
 
-function generarAmigoSecreto() {
-    return amigoSecreto =  Math.floor(Math.random()*amigos)+1;
+//Mostrar amigos en lista
+
+function mostrarLista(){
+let listaUl = document.getElementById("listaAmigos");
+listaUl.innerHTML = "";
+amigos.forEach((amigo) => {
+    let li = document.createElement("li");
+    li.textContent = amigo;
+    listaUl.appendChild(li);
+});
 }
 
-function limpiarCaja() {
-    document.querySelector('#amigo').value = '';
+
+//funcion para sortear amigos
+function sortearAmigo(){
+    let amigoAleatorio = sortearAmigo; //Amigo sorteado
+    if(amigos.length === 0){
+        alert("No hay amigos en la lista");
+        return;
+    }
+    amigoAleatorio = Math.floor(Math.random()*amigos.length);
+    let amigoSorteado = amigos[amigoAleatorio];
+    document.getElementById("resultado").innerHTML = `<li>Tu amigo secreto es: ${amigoSorteado}</li>`;
+    return;
 }
+
+//funcion limpiar
+function limpiarCaja(){
+    document.getElementById("amigo").value = "";
+    return;
+}
+
+
+
+//funcion para reiniciar
+/*function reiniciar(){
+    amigos = [];
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "";
+    return;
+}
+    */
